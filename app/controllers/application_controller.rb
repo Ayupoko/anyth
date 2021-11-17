6f02f2422695188640336856f590ea41ba469752
EC2 Default User <ec2-user@ip-172-31-48-173.ec2.internal>
2021-11-17 09:17:40 +0000

    README_edit2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :encrypted_password, :first_name, :last_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :image_id, :movie_id ])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [ :email, :encrypted_password, :first_name, :last_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :image_id, :movie_id ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :encrypted_password, :first_name, :last_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :image_id, :movie])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [ :email, :encrypted_password, :first_name, :last_name ])
   end
 end
